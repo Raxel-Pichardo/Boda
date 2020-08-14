7b0b3d5a066ec07612472f3b1da099dcd63f129a
Raxel Pichardo <raxel.pichardo@telefonica.com>
2020-08-14 13:27:44 -0500

    Cambios 140820

--- js/app.js
+++ js/app.js
@@ -630,7 +630,7 @@ var app = {
     },
    reloj: function () {
         setInterval(function () {
-            contador("2020-11-28T16:00:00.000Z");
+            contador("2020-11-28T21:00:00.000Z");
         }, 1000);
     },
    abrirRecomendacion: function (recomendacion) {
