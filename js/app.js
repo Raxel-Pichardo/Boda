4c53a2f5d3074825ed4b5449b03f3bdb19920bb8
Raxel Pichardo <raxel.pichardo@telefonica.com>
2020-09-03 02:56:42 -0500

    Agregando ajax para actualizar BD de confirmados

--- js/app.js
+++ js/app.js
@@ -3,6 +3,79 @@
 
 var url = window.location.search.replace(/\?/g, '');
 
+//INICIO FORMULARIO 
+
+      // Process contact form
+      $('#form-confirm').submit(function(event) {
+        console.log("Hola");
+    event.preventDefault();
+    setTimeout(function() {
+        // Get data
+      var data = {
+        'entry.1119495510': $("#form-name").val(),
+        'entry.1128010576': $("#form-email").val(),
+        'entry.240744114': $("#form-phone").val(),
+        'entry.1857846393': $("#invitados").val(),
+        'entry.996780921': $("#form-message").val()
+      };
+
+      console.log("El nombre es: " + $("#form-name").val());
+      console.log("El correo es: " + $("#form-email").val());
+      console.log("El teléfono es: " + $("#form-phone").val());
+      console.log("La respuesta es: " + $("#invitados").val());
+      console.log("El mensaje es: " + $("#form-message").val());
+      console.log(data[3]);
+      console.log($("#invitados").val());
+  
+      // Validate form
+      var formSuccess = true;
+      Object.keys(data).forEach(function(key, index) {
+        if (!data[key]) {
+          formSuccess = false;
+        }
+      });
+  
+      if (formSuccess) {
+        // Send request
+        $.ajax({
+          url: 'https://docs.google.com/forms/d/e/1FAIpQLSdeIyTl2Z5ufSAGpiBer0Sqzq5Fmnt4DGkzDBCx-oaxePl-cg/formResponse?vc=0&c=0&w=1&flr=0',
+          type: 'POST',
+          crossDomain: true,
+          dataType: "xml",
+          data: data,
+          beforeSend: function (data) {
+            $(".wrap-send").fadeIn();
+        },
+          success: function(jqXHR, textStatus, errorThrown) {
+            console.log("Fue exitoso");
+            setTimeout(function () {
+                $(".enviando").hide();
+                $(".wrap-send").html(
+                    "<h1>¡Muchas Gracias por tu confirmación, te esperamos en nuestra Boda!</h1>"
+                );
+            }, 3000);
+          },
+          error: function(jqXHR, textStatus, errorThrown) {
+            console.log("Fue error");
+            $(".enviando").hide();
+            if( $("#invitados").val() == "Asistiré"){
+                $(".wrap-send").html(
+                    "<h1>¡Muchas Gracias por tu confirmación, te esperamos en nuestra Boda!</h1>"
+                );
+            }
+            else if( $("#invitados").val() == "No Asistiré"){
+                $(".wrap-send").html(
+                    "<h1>¡Gracias por tu respuesta!</h1>"
+                );
+            }
+          }
+        });
+      }
+    }, 300);
+  });
+
+          //FIN FORMULARIO
+
 var app = {
     enviarConfirmacion: function (event) {
        event.preventDefault();
@@ -38,15 +111,15 @@ var app = {
                 setTimeout(function () {
                     $(".enviando").hide();
                     $(".wrap-send").html(
-                        "<h1>Â¡Muchas Gracias por tu confirmación te esperamos en nuestra Boda!</h1>"
+                        "<h1>¡Muchas Gracias por tu confirmación te esperamos en nuestra Boda!</h1>"
                     );
                 }, 3000);
             },
             error: function (respuesta) {
-                console.log(respuesta);
+                console.log("La respuesta es: " + respuesta);
                 $(".enviando").hide();
                 $(".wrap-send").html(
-                    "<h1>Â¡Muchas Gracias por tu confirmaciÃ³n te esperamos en nuestra Boda!</h1>"
+                    "<h1>¡Muchas Error Gracias por tu confirmación te esperamos en nuestra Boda!</h1>"
                 );
             }
         });
